d52fd9821e5de59e9b3b19e166dce1f46fd4782d
begidudimtse <begidudimtse@gmail.com>
2025-09-29 15:51:51 +0300

    edited

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -21,7 +21,7 @@ const Landing: React.FC = () => {
         <div className="flex gap-4">
           <Button title="Rounded-sm" styles="px-4 py-2 rounded-sm" />
           <Button title="Rounded-md" styles="px-4 py-2 rounded-md" />
-          <Button title="Rounded-full" styles="px-4 py-2 rounded-full" />
+          <Button title="Rounded-lg" styles="px-4 py-2 rounded-lg" />
         </div>
       </div>
     </div>
